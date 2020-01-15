b1be05c29464e88134011f0213e5bd668c22c704
David Cantoran <davidcantoran36@gmail.com>
2020-01-15 00:26:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :name
   attr_reader :id, :grade
   
-  def initialize(name, grade)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
   end 
