cfa052e6c592be5e390b56b7ae7add6dab719daa
David Cantoran <davidcantoran36@gmail.com>
2020-01-15 00:21:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,10 @@ class Student
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-      )
+      id INTEGER PRIMARY KEY,
+      name TEXT, 
+      grade TEXT 
+      );
     SQL 
   end 
   
